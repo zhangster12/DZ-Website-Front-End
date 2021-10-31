c3bd891c4379731b6b684d3997ccdcafd10771e4
Daniel Zhang <zhangster12@gmail.com>
2021-10-31 13:37:18 -0400

    Fixed dark mode

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -3,14 +3,14 @@
 import React from 'react';
 
 const DarkMode = () => {
-    let clickedClass = "clicked";
+    let clickedClass = 'clicked';
     const body = document.body;
-    const lightTheme = "light";
-    const darkTheme = "dark";
+    const lightTheme = 'light';
+    const darkTheme = 'dark';
     let theme;
 
     if (localStorage) {
-        theme = localStorage.getItem("theme");
+        theme = localStorage.getItem('theme');
     }
 
     if (theme === lightTheme || theme === darkTheme) {
@@ -20,15 +20,17 @@ const DarkMode = () => {
     }
 
     const switchTheme = (e) => {
+
         if (theme === darkTheme) {
             body.classList.replace(darkTheme, lightTheme);
             e.target.classList.remove(clickedClass);
-            localStorage.setItem("theme", "light");
+            localStorage.setItem('theme', 'light');
             theme = lightTheme;
+
         } else {
             body.classList.replace(lightTheme, darkTheme);
             e.target.classList.add(clickedClass);
-            localStorage.setItem("theme", "dark");
+            localStorage.setItem('theme', 'dark');
             theme = darkTheme;
         }
     };
