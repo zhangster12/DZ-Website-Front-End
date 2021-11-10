93135352997b7de1fcdd62996104313c4d6f0170
Daniel Zhang <zhangster12@gmail.com>
2021-11-10 13:04:56 -0500

    Edited file

--- src/content/AboutPageContent.js
+++ src/content/AboutPageContent.js
@@ -2,12 +2,12 @@ let AboutPageContent = {
     about: [{
         name: 'About Me',
         desc: [
-            'I was a flight controls engineer at Lockheed Martin in Patuxent River, Maryland, and I graduated from the Georgia Institute of Technology with a degree in Aerospace Engineering on May 2019. I created this website during the Covid-19 pandemic, because I wanted to learn new skills and have a new avenue for self-expression and creativity.',
-            'Since I was young, I’ve always been fascinated by space exploration and science. My interest was sparked when I visited the Smithsonian Udvar Hazy Air and Space Museum, where learned about the Opportunity and Spirit Mars rovers. Since then, I’ve continued learning more and majored in Aerospace Engineering. Now, I’m looking to conduct research, so I can contribute to developing new technologies.',
+            'I was a flight controls engineer at Lockheed Martin, and I graduated from the Georgia Institute of Technology with a degree in Aerospace Engineering on May 2019. I created this website during the Covid-19 pandemic, because I wanted to learn new skills and have a new avenue for self-expression and creativity.',
+            'Since I was young, I\'ve always been fascinated by space exploration and science. My interest was sparked when I visited the Smithsonian Udvar Hazy Air and Space Museum, where learned about the Opportunity and Spirit Mars rovers. Since then, I\'ve continued learning more and majored in Aerospace Engineering. Since the pandemic, I\'ve become more interested in biology and aging, and now, I wish to conduct research and develop new technologies.',
             'I also like to keep myself busy by gaining knowledge such as programming. Despite only taking a few programming courses at Georgia Tech, I enjoy learning about it and other subjects.',
-            'Outside of work or learning to code, I enjoy boxing, playing guitar, and swimming. I’ve competed in swimming throughout my life and participated in college club swimming, where we are the Champ Champs. I started boxing during my third year in college, and I’ve loved it ever since.',
+            'Outside of work or learning to code, I enjoy boxing, playing guitar, and swimming. I\'ve competed in swimming throughout my life and participated in college club swimming, where we are the Champ Champs. I started boxing during my third year in college, and I\'ve loved it ever since.',
             'My goals are to constantly learn, solve problems, and take those lessons to help and share with others.',
-            'I’m always excited to hear from everyone, so please don’t hesitate to reach out! Thanks for checking my website out!'
+            'I\'m always excited to hear from everyone, so please don\'t hesitate to reach out! Thanks for checking my website out!'
         ],
         id: 1
 }]};
